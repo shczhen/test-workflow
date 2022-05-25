4679ed3931fb395b8d0398c483bdca597d37b1f4
czhen <56986964+shczhen@users.noreply.github.com>
2022-05-25 13:39:00 +0800

    update

--- docsearch/run-algolia-crawler-incrementally.sh
+++ docsearch/run-algolia-crawler-incrementally.sh
@@ -7,4 +7,4 @@ echo "===test==="
 echo "CONFIG=$(cat $1/algolia_configs/zh-tidb-v6.0.json | jq -r tostring)" 
 
 # crawl zh tidb
-docker run --rm --env-file="$1/.env" -e "CONFIG=$(cat $1/algolia_configs/zh-tidb-v6.0.json | jq -r tostring)" -e  "ISINCREMENTAL=True" -v $1/algolia_configs:/data $DOCKER_REGISTRY/algolia-docsearch-scraper-incremental:v0.2
+docker run --rm --env-file="$1/.env" -e 'CONFIG=$(cat $1/algolia_configs/zh-tidb-v6.0.json | jq -r tostring)' -e  "ISINCREMENTAL=True" -v $1/algolia_configs:/data $DOCKER_REGISTRY/algolia-docsearch-scraper-incremental:v0.2
